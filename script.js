120bc8e9b22855ab56e35800669dfb191618edc4
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-05-02 22:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-};
+	const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+const firstDate = new Date(date1);
+const secondDate = new Date(date2);
+
+const diffDays = Math.round((secondDate-firstDate) / oneDay);
+	
+	return diffDays;	
+}
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
